5e192432cf2584f4f8e86c044cddec02918292ec
juhipark <juhipark@student.hartnell.edu>
2018-01-27 23:23:49 -0800

    pulling

--- ComeThru/app/src/main/java/com/example/danielmorales/comethru/MapsActivity.java
+++ ComeThru/app/src/main/java/com/example/danielmorales/comethru/MapsActivity.java
@@ -41,25 +41,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        buildGoogleApiClient();
+        mMap.setMyLocationEnabled(true);
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng seaside = new LatLng(36.614922, -121.822098);
-        mMap.addMarker(new MarkerOptions().position(seaside).title("Marker in Seaside"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seaside, 15));
-
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 
     @Override

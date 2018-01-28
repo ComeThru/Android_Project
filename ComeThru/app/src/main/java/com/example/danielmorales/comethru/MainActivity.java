e15ede0e535d2e5c306067319f9029e07d6c1f8c
Samuel Peters <helllo57748@comcast.net>
2018-01-27 19:23:47 -0800

    added

--- ComeThru/app/src/main/java/com/example/danielmorales/comethru/MainActivity.java
+++ ComeThru/app/src/main/java/com/example/danielmorales/comethru/MainActivity.java
@@ -10,36 +10,10 @@ import android.content.Intent;
 import java.sql.Driver;
 
 public class MainActivity extends AppCompatActivity {
-    private Button mDriver, mCustomer;
-    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mDriver = (Button) findViewById(R.id.Driver);
-        mCustomer = (Button) findViewById(R.id.Customer);
-        intent = new Intent(MainActivity.this, SingIn.class);
-
-        mDriver.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                intent.putExtra("person", 0);
-                startActivity(intent);
-                finish();
-                return;
-            }
-        });
-
-        mCustomer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                intent.putExtra("person", 1);
-                startActivity(intent);
-                finish();
-                return;
-            }
-        });
     }
 }
--- ComeThru/app/src/main/java/com/example/danielmorales/comethru/SingIn.java
+++ ComeThru/app/src/main/java/com/example/danielmorales/comethru/SingIn.java
@@ -4,18 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SingIn extends AppCompatActivity {
 
     private int person;
     private TextView register;
+    private Button sign_in_button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing_in);
         TextView registerButtton = (TextView) findViewById(R.id.register);
+        Button sign_in_button = (Button) findViewById(R.id.signInButton);
 
         Intent intent = getIntent();
         person = intent.getIntExtra("person", 0);
@@ -30,9 +34,17 @@ public class SingIn extends AppCompatActivity {
                 }else{
                     ;
                 }
+
             }
         });
 
+        sign_in_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SingIn.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 

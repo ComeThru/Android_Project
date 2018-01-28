806a2efe3f47d50998be66467ad79cbb5f5ed140
juhipark <juhipark@student.hartnell.edu>
2018-01-27 21:11:31 -0800

    Created map activity and linked it to sign in button

--- ComeThru/app/src/main/java/com/example/danielmorales/comethru/MapsActivity.java
+++ ComeThru/app/src/main/java/com/example/danielmorales/comethru/MapsActivity.java
@@ -39,8 +39,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng seaside = new LatLng(36.614922, -121.822098);
+        mMap.addMarker(new MarkerOptions().position(seaside).title("Marker in Seaside"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seaside,15));
+
     }
 }

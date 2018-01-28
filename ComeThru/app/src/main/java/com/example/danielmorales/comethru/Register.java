68724256d9237ec5fb95bcea706b39050473a129
Samuel Peters <helllo57748@comcast.net>
2018-01-27 20:18:21 -0800

    linking

--- ComeThru/app/src/main/java/com/example/danielmorales/comethru/Register.java
+++ ComeThru/app/src/main/java/com/example/danielmorales/comethru/Register.java
@@ -2,12 +2,20 @@ package com.example.danielmorales.comethru;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
+
+    private Button consumer_button;
+    private Button ice_cream_man;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        Button consumer_button = (Button) findViewById(R.id.)
+
     }
 }
